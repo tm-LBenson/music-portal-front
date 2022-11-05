434b902d825911bd4704ed51e136690d832cb9e3
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-11-05 10:10:31 -0400

    create home component, and splash component

--- src/App.js
+++ src/App.js
@@ -4,38 +4,15 @@ import './components/stylesheets/home.css'
 import NavDrawer from './components/nav/NavDrawer';
 import NavArea from './components/nav/NavArea'
 import React from 'react';
-import HeroTagLine from './components/home/HeroTagLine';
+import Home from './components/home/Home'
+import SplashPage from './components/splashPage/SplashPage' //TODO conditional rendering after login
 function App() {
   return (
     <React.Fragment>
+
       <NavDrawer />
-      
       <NavArea />
-      <main className='wrapper'>
-        <section className='col-1'>
-          <div className='video'></div>
-        </section>
-        <section className='col-2'>
-          <HeroTagLine />
-          <div className='hero__trending'>TRENDING</div>
-          <section className='cards'>
-            <div className='card'></div>
-            <div className='card'></div>
-            <div className='card'></div>
-            <div className='card'></div>
-            <div className='card'></div>
-            <div className='card'></div>
-
-          </section>
-        </section>
-        <section className='col-3'>
-          <div className='search'>Search</div>
-          <div className='play-list'>playlist</div>
-        </section>
-
-
-      </main>
-
+      <Home />
     </React.Fragment>
   );
 }
---
+++ src/components/home/Home.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react'
+import HeroTagLine from './HeroTagLine'
+export default class Home extends Component {
+  render() {
+    return (
+      <main className='wrapper'>
+        <section className='col-1'>
+          <div className='video'></div>
+        </section>
+        <section className='col-2'>
+          <HeroTagLine />
+          <div className='hero__trending'>TRENDING</div>
+          <section className='cards'>
+            <div className='card'></div>
+            <div className='card'></div>
+            <div className='card'></div>
+            <div className='card'></div>
+            <div className='card'></div>
+            <div className='card'></div>
+
+          </section>
+        </section>
+        <section className='col-3'>
+          <div className='search'>Search</div>
+          <div className='play-list'>playlist</div>
+        </section>
+
+
+      </main>
+    )
+  }
+}
