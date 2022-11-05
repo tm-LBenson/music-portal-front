e633c5f4985d92902085015be25b91178405fd6c
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-11-04 23:36:34 -0400

    layout homepage

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './components/stylesheets/reset.css'
 import './components/stylesheets/App.css'
+import './components/stylesheets/home.css'
 import NavDrawer from './components/nav/NavDrawer';
 import NavArea from './components/nav/NavArea'
 import React from 'react';
@@ -8,8 +9,33 @@ function App() {
   return (
     <React.Fragment>
       <NavDrawer />
+      
       <NavArea />
-      <HeroTagLine />
+      <main className='wrapper'>
+        <section className='col-1'>
+          <div className='video'></div>
+        </section>
+        <section className='col-2'>
+          <HeroTagLine />
+          <div className='hero__trending'>TRENDING</div>
+          <section className='cards'>
+            <div className='card'></div>
+            <div className='card'></div>
+            <div className='card'></div>
+            <div className='card'></div>
+            <div className='card'></div>
+            <div className='card'></div>
+
+          </section>
+        </section>
+        <section className='col-3'>
+          <div className='search'>Search</div>
+          <div className='play-list'>playlist</div>
+        </section>
+
+
+      </main>
+
     </React.Fragment>
   );
 }
