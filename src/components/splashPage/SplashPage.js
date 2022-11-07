001817e3c9df629215eef43d435d62632762705e
Zoe Gonzalez <zusolaris.personal@gmail.com>
2022-11-07 14:11:36 -0800

    login card

--- src/components/splashPage/SplashPage.js
+++ src/components/splashPage/SplashPage.js
@@ -1,7 +1,7 @@
 import styles from '../stylesheets/SplashPage.module.css'
 import React, { Component } from 'react'
 import { Card } from 'react-bootstrap'
-
+import Button from 'react-bootstrap/Button';
 
 const CLIENT_ID = '75b90235ba3f4956834f605c46d9d923'
 const REDIRECT_URI = 'http://localhost:3000/'
@@ -18,13 +18,18 @@ export default class SplashPage extends Component {
   render() {
 
     return (
+      <>
 <div >
 <Card className={styles['login']}>
-      <main className={styles.main}>
-        <h1><a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}>Login to Spotify</a></h1>
-      </main >
+  <Card.Header>Login to Portal via Spotify</Card.Header>
+      <Card.Body>
+
+        <Button variant= "primary" className={styles['button']} href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}&scope=${SCOPE}`}>Link your Spotify</Button>
+        </Card.Body>
       </Card>
       </div>
+   
+      </>
 
     )
   }
