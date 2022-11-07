588b6a8b9d097a0bd6848f329eb65d67dd13ec9e
Zoe Gonzalez <zusolaris.personal@gmail.com>
2022-11-07 11:38:24 -0800

    added forms and components and logout feature

--- src/components/music-portal/MusicPortal.js
+++ src/components/music-portal/MusicPortal.js
@@ -1,9 +1,48 @@
 import React, { Component } from 'react'
+import PortalForm from './Portalsearch'
+import UserForm from './Usertop25'
+import UserTop from './UserResults'
+import Song from '../../Song'
+import styles from '../stylesheets/MusicPortal.module.css'
+import Searchbar from '../home/Searchbar'
+import { Container } from 'react-bootstrap'
 
 export default class MusicPortal extends Component {
   render() {
     return (
-      <div>MusicPortal</div>
+      <main className={styles['wrapper']}>
+        <section className={styles['col-1']}>
+      <div>Column1</div>
+      <div>
+      <UserForm />
+<div className={styles['play-list']}><UserTop /></div>
+  
+    </div>
+      </section>
+      <section className={styles['col-2']}>
+      <div>Column2</div>
+      <div className={styles['col-2-1']}>
+      < PortalForm />
+      
+
+      <section className={styles['cards']}>
+            <div className={styles['card']}>< Song /></div>
+            <div className={styles['card']}>< Song /></div>
+            <div className={styles['card']}>< Song /></div>
+            <div className={styles['card']}>< Song /></div>
+            <div className={styles['card']}>< Song /></div>
+            <div className={styles['card']}>< Song /></div>
+          </section>
+      </div>
+      </section>
+      <section className={styles['col-3']}>
+      <Container className={styles['placeholder']}>Place Holder</Container>
+      <Container className={styles['placeholder']}>Place Holder</Container>
+      <Container className={styles['placeholder']}>Place Holder</Container>
+      </section>
+      
+      
+</main>
     )
   }
 }
