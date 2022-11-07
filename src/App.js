41ad93ece5ab4d16089aa377cc4f8c1bc4c65ff1
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-11-07 11:08:36 -0500

    create new routes

--- src/App.js
+++ src/App.js
@@ -6,9 +6,11 @@ import './components/stylesheets/home.css'
 import NavDrawer from './components/nav/NavDrawer';
 import NavArea from './components/nav/NavArea'
 import Home from './components/home/Home'
+import About from './components/about/About'
+import MusicPortal from './components/music-portal/MusicPortal'
 import SplashPage from './components/splashPage/SplashPage'
 import Auth from './Auth';
-
+import { Route, Routes } from 'react-router-dom';
 
 const code = new URLSearchParams(window.location.search).get("code")
 
@@ -33,16 +35,32 @@ export default class App extends Component {
     return (
       <React.Fragment>
 
-        {!code ? <SplashPage checkLogin={this.checkLogin} /> :
 
-          <React.Fragment >
-            {!this.state.token ? <Auth onGetToken={this.getToken} code={code} /> : null}
-            <NavDrawer />
-            {this.state.token ? <NavArea token={this.state.token} /> : null}
-            <Home />
-          </React.Fragment>
-        }
+        <Routes>
+          <Route path='/' element={
+            !code ? <SplashPage checkLogin={this.checkLogin} /> :
+              <React.Fragment >
+                {!this.state.token ? <Auth onGetToken={this.getToken} code={code} /> : null}
+                <NavArea />
+                {this.state.token ? <NavDrawer token={this.state.token} /> : null}
+                <Home />
+              </React.Fragment>
+          } />
 
+          <Route path='/about' element={
+            <React.Fragment>
+              {this.state.token ? <NavDrawer token={this.state.token} /> : null}
+              < About />
+            </React.Fragment>
+          } />
+          <Route path='/music-portal' element={
+            <React.Fragment >
+              {this.state.token ? <NavDrawer token={this.state.token} /> : null}
+              < MusicPortal />
+            </React.Fragment>
+          } />
+
+        </Routes>
 
       </React.Fragment>
     )
