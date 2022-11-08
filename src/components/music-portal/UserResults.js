b5d5952bf4dc21a5961de2e03561243e716b36ba
Stevenrej <steveo732@gmail.com>
2022-11-07 19:35:14 -0600

    fix artist bug

--- src/components/music-portal/UserResults.js
+++ src/components/music-portal/UserResults.js
@@ -7,8 +7,10 @@ export default class Results extends Component {
   render() {
 
     let topResults = this.props.topUserData.map(item => {
-      console.log(item.artists)
-      return <UserOutput ArtistName={item.name}TrackArtist={item.artists[0].name}/>
+      console.log(item)
+      return  item.duration_ms ? <UserOutput ArtistName={item.name} TrackArtist={item.artists[0].name}/> : <UserOutput ArtistName={item.name} TrackArtist=''/>
+     
+      
     })
     
 
