74f5374ddf383ba95620ee221f0227d156e62cad
Lewis Benson <tm.lewis.benson@gmail.com.com>
2022-11-09 12:06:37 -0500

    add play list function for player

--- src/components/home/Playlist.js
+++ src/components/home/Playlist.js
@@ -3,15 +3,42 @@ import Card from 'react-bootstrap/Card';
 import styles from '../stylesheets/Song.module.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faPlay } from '@fortawesome/free-solid-svg-icons'
-
+import axios from 'axios';
 
 export default class Song extends Component {
   constructor() {
     super()
     this.state = {}
   }
+  playFromDailyPlaylist = async (uri) => {
+    try {
+      console.log(this.props)
+      console.log('getting data')
+      const data = await axios({
+        method: 'put', //you can set what request you want to be
+        url: 'https://api.spotify.com/v1/me/player/play',
+        data: {
+          "context_uri": uri,
+          "offset": {
+            "position": 5
+          },
+          "position_ms": 0
+        },
+        headers: {
+          'Authorization': 'Bearer ' + this.state.token,
+          'accept': 'application/json',
+          'Content-type': 'application/json',
+        }
+      })
+      this.setState({ featured: data.data }, () => this.props.passDataUp(this.state.featured))
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+  handleClick = (e) => {
+    console.log(e.target.value)
 
-
+  }
 
 
   render() {
@@ -25,12 +52,20 @@ export default class Song extends Component {
             <Card.Title>
               {this.props.songData.name}
             </Card.Title>
-            <Card.Text className={styles['card-text']}>
-              <FontAwesomeIcon icon={faPlay} />
-            </Card.Text>
+            <div className={styles['card__text-wrapper']}>
+              <div className={styles['card__text-content']}>
+                <button className={styles['card__text-content--button']} onClick={this.handleClick} value={this.props.songData.uri}></button>
+                <Card.Text className={styles['card-text']}>
+                  <FontAwesomeIcon icon={faPlay} />
+
+                </Card.Text>
+              </div>
+            </div>
+
+
           </Card.Body>
         </Card>
-      </React.Fragment>
+      </React.Fragment >
     )
   }
 }
