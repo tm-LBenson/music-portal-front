b9543b45eb8ccfde151d6c9d8bc3a9c6394ac091
Trace Dugar <tracedugar89@gmail.com>
2022-11-10 13:51:07 -0800

    ADD Team Photos in cards on About, ADD Trace PLaylist on About

--- src/components/about/DeveloperLewis.js
+++ src/components/about/DeveloperLewis.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import pic from './AboutAssests/Lewis.jpg';
+import styles from '../stylesheets/About.module.css';
 
 export default class DeveloperLewis extends Component {
   render() {
     return (
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" className={styles['img']} src={pic}/>
         <Card.Body>
           <Card.Title>Lewis</Card.Title>
           <Button variant="success">About</Button>
--- src/components/about/DeveloperZoe.js
+++ src/components/about/DeveloperZoe.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
+import pic from './AboutAssests/Zoe.jpg';
+import styles from '../stylesheets/About.module.css';
 
 export default class DeveloperZoe extends Component {
   render() {
     return (
       <Card style={{ width: '18rem' }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" className={styles['img']} src={pic}/>
         <Card.Body>
           <Card.Title>Zoe</Card.Title>
           <Button variant="success">About</Button>
