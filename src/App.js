0032e5d06742842ac7b359ee72103f361da4c273
Lewis Benson <105423307+tm-LBenson@users.noreply.github.com>
2023-01-21 23:49:37 -0500

    add comments to code

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, { Component } from 'react'
+
+// Import stylesheets
 import './components/stylesheets/reset.css'
 import './components/stylesheets/App.css'
+
+// Import other components
 import NavDrawer from './components/nav/NavDrawer';
 import NavArea from './components/nav/NavArea'
 import Home from './components/home/Home'
@@ -9,42 +13,58 @@ import MusicPortal from './components/music-portal/MusicPortal'
 import SplashPage from './components/splashPage/SplashPage'
 import Footer from './components/nav/Footer'
 import Auth from './Auth';
+
+// Import react-router-dom library for handling routes
 import { Route, Routes } from 'react-router-dom';
+
+// Import axios library for making HTTP requests
 import axios from 'axios';
+
 import './components/stylesheets/music-player.css'
+
 const code = new URLSearchParams(window.location.search).get("code")
 
+// Define App component
 export default class App extends Component {
   constructor() {
     super()
+    // Initialize component's state
     this.state = {
-      token: '',
-      data: {},
-      topData: []
+      token: '', // token used for authentication
+      data: {}, // data to be passed between components
+      topData: [] // data related to top tracks/artists
     }
   }
 
+  // Method to update token state
   checkLogin = (token) => {
     this.setState({ token: token })
   }
 
+  // Method to update token state
   getToken = (token) => {
     this.setState({ token: token })
   }
 
+  // Method to update topData state
   handleData = (topData) => {
     this.setState({ topData: topData })
   }
 
+  // Method to update user_id state
   getUserID = (userid) => {
     this.setState({ user_id: userid })
   }
+
+  // Method to update currentlyPlaying state
   grabPlayingStatus = (isItPlaying) => {
     this.setState({ currentlyPlaying: isItPlaying })
   }
+
+  // Method to update getSong state
   passFunction = (func) => this.setState({ getSong: func })
 
-
+  // Method to post user data to the server
   postUserData = async (obj) => {
     try {
       let url = `${process.env.REACT_APP_SERVER}/user-results`;
@@ -61,6 +81,8 @@ export default class App extends Component {
   }
 
   trackUri = 'spotify:artist:3HkwxR8PuBf5hvumgsfByJ'
+
+  // Render method to render components based on URL route
 
   render() {
     console.log(this.state.user_id)
